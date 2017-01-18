a65204e994b6626671d670347a0d5902ac08012f
elmadjian <cadu_elmadjian@yahoo.com.br>
2017-01-18 19:10:19 -0200

    process initialization

--- augmented_reality/main.py
+++ augmented_reality/main.py
@@ -1,8 +1,12 @@
-import cv2, marker_detector
+import cv2, marker_detector, sys
 
 def main():
     md = marker_detector.MarkerDetector()
     video = cv2.VideoCapture(0)
+    # img = cv2.imread(sys.argv[1])
+    # img = md.detect(img)
+    # cv2.imshow("teste", img)
+    # cv2.waitKey(0)
     while True:
         frame = video.read()[1]
         img = md.detect(frame)
--- augmented_reality/marker_detector.py
+++ augmented_reality/marker_detector.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 class MarkerDetector():
 
@@ -7,20 +8,41 @@ class MarkerDetector():
 
     def detect(self, img):
         preprocessed_img = self._preprocess(img)
+        contour_list     = self._find_contours(preprocessed_img)
+        self._find_candidates(contour_list, img)
         return preprocessed_img
 
     def _preprocess(self, img):
         filtered_img = cv2.medianBlur(img, 5)
         gray_img = cv2.cvtColor(filtered_img, cv2.COLOR_BGR2GRAY)
-        return  cv2.adaptiveThreshold(gray_img, 255,
-                                        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                        cv2.THRESH_BINARY_INV, 7, 7)
+        return cv2.adaptiveThreshold(gray_img, 255,
+                                      cv2.ADAPTIVE_THRESH_MEAN_C,
+                                      cv2.THRESH_BINARY, 9, 9)
 
     def _find_contours(self, img):
         contours = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[1]
-        #for c in contours:
+        contour_list = []
+        # temp = np.zeros(img.shape, dtype="uint8")
+        for c in contours:
+            if len(c) > 5:
+                contour_list.append(c)
+                # cv2.drawContours(temp, [c], 0, (255, 255, 255), 2)
+                # cv2.imshow("teste", temp)
+                # cv2.waitKey(0)
+        return contour_list
 
 
+    def _find_candidates(self, contour_list, img):
+        # temp = np.zeros(img.shape, dtype="uint8")
+        for c in contour_list:
+            eps = cv2.arcLength(c, True) * 0.05
+            approx = cv2.approxPolyDP(c, eps, True)
+            # print(len(approx))
+            if len(approx) != 4:
+                continue
+            if not cv2.isContourConvex(approx):
+                continue
 
-    def _find_markers(self):
-        pass
+                # cv2.drawContours(temp, [approx], 0, (255,255,255), 2)
+                # cv2.imshow("teste", temp)
+                # cv2.waitKey(0)
