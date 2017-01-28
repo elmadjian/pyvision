664d0d589dfb48cad7ebba9c4b98ad8e99abfb04
elmadjian <cadu_elmadjian@yahoo.com.br>
2017-01-28 15:08:44 -0200

    adding image transform

--- augmented_reality/main.py
+++ augmented_reality/main.py
@@ -2,16 +2,16 @@ import cv2, marker_detector, sys
 
 def main():
     md = marker_detector.MarkerDetector()
-    video = cv2.VideoCapture(0)
-    # img = cv2.imread(sys.argv[1])
-    # img = md.detect(img)
-    # cv2.imshow("teste", img)
-    # cv2.waitKey(0)
-    while True:
-        frame = video.read()[1]
-        img = md.detect(frame)
-        cv2.imshow("teste", img)
-        cv2.waitKey(10)
+    #video = cv2.VideoCapture(0)
+    img = cv2.imread(sys.argv[1])
+    img = md.detect(img)
+    cv2.imshow("teste", img)
+    cv2.waitKey(0)
+    # while True:
+    #     frame = video.read()[1]
+    #     img = md.detect(frame)
+    #     cv2.imshow("teste", img)
+    #     cv2.waitKey(10)
 
 
 if __name__=="__main__":
--- augmented_reality/marker_detector.py
+++ augmented_reality/marker_detector.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, sys
 import numpy as np
 
 class MarkerDetector():
@@ -9,7 +9,9 @@ class MarkerDetector():
     def detect(self, img):
         preprocessed_img = self._preprocess(img)
         contour_list     = self._find_contours(preprocessed_img)
-        self._find_candidates(contour_list, img)
+        possible_markers = self._find_candidates(contour_list, img)
+        transformed_ones = self._transform_marker(possible_markers, img)
+        
         return preprocessed_img
 
     def _preprocess(self, img):
@@ -22,27 +24,68 @@ class MarkerDetector():
     def _find_contours(self, img):
         contours = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[1]
         contour_list = []
-        # temp = np.zeros(img.shape, dtype="uint8")
         for c in contours:
             if len(c) > 5:
                 contour_list.append(c)
-                # cv2.drawContours(temp, [c], 0, (255, 255, 255), 2)
-                # cv2.imshow("teste", temp)
-                # cv2.waitKey(0)
         return contour_list
 
 
     def _find_candidates(self, contour_list, img):
-        # temp = np.zeros(img.shape, dtype="uint8")
+        possible_markers = []
         for c in contour_list:
             eps = cv2.arcLength(c, True) * 0.05
             approx = cv2.approxPolyDP(c, eps, True)
-            # print(len(approx))
             if len(approx) != 4:
                 continue
             if not cv2.isContourConvex(approx):
                 continue
+            if self._is_min_length(approx, 1000):
+                continue
+            v1 = (approx[1] - approx[0])[0]
+            v2 = (approx[2] - approx[0])[0]
+            lr = (v1[0] * v2[1]) - (v1[1] * v2[0])
+            if lr < 0.0:
+                approx[1], approx[3] = approx[3], approx[1]
+            possible_markers.append(approx)
+        return possible_markers
 
+
+    def _is_min_length(self, approx, min_length):
+        min_dist = sys.maxsize
+        for i in range(4):
+            points = approx[i] - approx[(i+1)%4]
+            squared_length = np.dot(points, points.T)
+            min_dist = min(min_dist, squared_length[0][0])
+        if min_dist < min_length:
+            return True
+        return False
+
+
+    def _transform_marker(self, candidates, img):
+        markers = []
+        for m in candidates:
+            (m0, m1, m2, m3) = m
+            m0, m1, m2, m3 = m0[0], m1[0], m2[0], m3[0]
+            new_m = np.array([m0, m1, m2, m3], dtype="float32")
+            side0 = np.sqrt((m0[0]-m1[0])**2 + (m0[1]-m1[1])**2)
+            side1 = np.sqrt((m1[0]-m2[0])**2 + (m1[1]-m2[1])**2)
+            side2 = np.sqrt((m2[0]-m3[0])**2 + (m2[1]-m3[1])**2)
+            side3 = np.sqrt((m3[0]-m0[0])**2 + (m3[1]-m0[1])**2)
+            side  = int(max(side0, side1, side2, side3))
+            dst = np.array([
+                [0,0],
+                [side-1, 0],
+                [side-1, side-1],
+                [0, side-1]
+            ], dtype="float32")
+            M = cv2.getPerspectiveTransform(new_m, dst)
+            warped = cv2.warpPerspective(img, M, (side, side))
+            markers.append(warped)
+        return markers
+            # cv2.imshow("teste", warped)
+            # cv2.waitKey(0)
+
+                # temp = np.zeros(img.shape, dtype="uint8")
                 # cv2.drawContours(temp, [approx], 0, (255,255,255), 2)
                 # cv2.imshow("teste", temp)
                 # cv2.waitKey(0)
